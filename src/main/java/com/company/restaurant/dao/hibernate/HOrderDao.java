647d7671c039b5480cd50af90741d10b3de852a6
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-13 01:11:36 +0300

    Restaurant  Model Dao Hibernate

---
+++ src/main/java/com/company/restaurant/dao/hibernate/HOrderDao.java
@@ -0,0 +1,71 @@
+package com.company.restaurant.dao.hibernate;
+
+import com.company.restaurant.dao.OrderDao;
+import com.company.restaurant.dao.hibernate.proto.HDaoEntity;
+import com.company.restaurant.model.Order;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by Yevhen on 13.06.2016.
+ */
+public class HOrderDao extends HDaoEntity<Order> implements OrderDao {
+    private static final String STATE_TYPE_ATTRIBUTE_NAME = "stateType";
+    private static final String ORDER_NUMBER_ATTRIBUTE_NAME = "orderNumber";
+
+    @Override
+    protected void initMetadata() {
+
+    }
+
+    @Override
+    public String orderEntityName() {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public Order addOrder(Order order) {
+        return save(order);
+    }
+
+    @Transactional
+    @Override
+    public String delOrder(Order order) {
+        return delete(order);
+    }
+
+    @Transactional
+    @Override
+    public Order findOrderById(int orderId) {
+        return findObjectById(orderId);
+    }
+
+    @Transactional
+    @Override
+    public List<Order> findOrderByNumber(String orderNumber) {
+        return findObjectsByAttributeValue(ORDER_NUMBER_ATTRIBUTE_NAME, orderNumber);
+    }
+
+    @Transactional
+    @Override
+    public List<Order> findAllOrders() {
+        return findAllObjects();
+    }
+
+    @Transactional
+    @Override
+    public List<Order> findAllOrders(String stateType) {
+        return findObjectsByAttributeValue(STATE_TYPE_ATTRIBUTE_NAME, stateType);
+    }
+
+    @Transactional
+    @Override
+    public Order updOrderState(Order order, String stateType) {
+        order.setStateType(stateType);
+        getCurrentSession().update(order);
+
+        return order;
+    }
+}
